1b9df7592100e670f3321083483b4eda5f0095cf
yuriuser126 <syuri5458@naver.com>
2025-05-21 16:31:12 +0900

    import DefectCsvcontroller 수정

--- src/main/java/com/boot/controller/DefectCsvController.java
+++ src/main/java/com/boot/controller/DefectCsvController.java
@@ -10,8 +10,11 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boot.dto.Defect_DetailsDTO;
