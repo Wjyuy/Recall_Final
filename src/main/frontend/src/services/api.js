1411efe1bbd95fc53aa37ad8a75c36f145a9a04f
Chaeyoon-k <codbs9265@gmail.com>
2025-05-21 16:03:23 +0900

    시도3

--- src/main/frontend/src/services/api.js
+++ src/main/frontend/src/services/api.js
@@ -5,8 +5,10 @@ import axios from 'axios';
 const API_BASE_URL = 'https://recall-final-backend.onrender.com/api';
 // const API_BASE_URL = window.location.hostname == 'localhost' ? 'http://localhost:8485/api' : 'https://recall-final-backend.onrender.com/api';
 export const fetchTestData = async () => {
+   console.log("axios 호출 시작");
   try {
     const response = await axios.get(`${API_BASE_URL}/test`);
+    console.log("받은 데이터: ", response.data);
     return response.data;
   } catch (error) {
     console.error('데이터를 가져오는 데 실패했습니다:', error);
